18863cd9d05d8b3f78fdd59acaa9146950e4e9ec
Da Kim <da.kim.9yb@vmcloud.local>
2023-02-07 12:50:27 -0800

    Section 6 - 7

--- Section 6/fastapienv/Scripts/activate.bat
+++ Section 6/fastapienv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\da.kim\Documents\FastAPI\fastapienv
+set VIRTUAL_ENV=C:\Users\da.kim.9yb\Documents\Learn\FastAPI\Section 6\fastapienv
 
 if not defined PROMPT set PROMPT=$P$G
 
